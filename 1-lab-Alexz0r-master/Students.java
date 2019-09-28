c6d03189cb70b45654e446ffc1bcb53205c63d60
Rino095 <rino095@mail.ru>
2019-09-28 11:32:10 +0300

    upd

---
+++ 1-lab-Alexz0r-master/Students.java
@@ -0,0 +1,9 @@
+class Students {
+	public static void main(String[] args) {
+		Student student = new Student();
+		student.age = 42;
+		student.name = "Tim";
+		student.isAlive = true;
+		System.out.println(student);
+	}
+}
